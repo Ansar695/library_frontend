27f3e9d87b39f6c900a74229ab5fb44151469e58
unknown <ansarsaeed988@gmail.com>
2022-07-25 12:13:05 +0500

    updated

--- src/Redux/features/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { productReducer, selectedProductReducer } from "./reducers";
-
-export const rootReducer = combineReducers({
-    allProducts: productReducer,
-    product: selectedProductReducer
-})
-
