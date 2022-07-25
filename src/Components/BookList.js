ded982cc1ed4423736ccee35b7d898be65368fd6
unknown <ansarsaeed988@gmail.com>
2022-07-25 12:48:21 +0500

    update

--- src/Components/BookList.js
+++ src/Components/BookList.js
@@ -1,8 +1,5 @@
 import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
 import BookComponent from './BookComponent'
-import axios from "axios"
-import { setProducts } from '../Redux/features/actions'
 
 const ProductListing = () => {
   return (
